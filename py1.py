e2ca41154e01383a6306ea0a06c5540d177a6623
KacperGrabiec <grabiec.kacper01@gmail.com>
2021-10-10 20:16:47 +0200

    add calc_sqr_vec_len() function

--- py1.py
+++ py1.py
@@ -3,12 +3,16 @@ def load_data(filename: str):
     data = []
     for line in file:
         line = line.strip()
-        data.append([int(ch) for ch in line.split(' ')])
+        data.append([int(value) for value in line.split(' ')])
     file.close()
     return data
+
+
+def calc_sqr_vec_len(vector):
+    return pow(vector[0], 2) + pow(vector[1], 2)
 
 
 data = load_data('in1.txt')
 n = data[0][0]
 moves = data[1:]
-print(n)
+start = [0, 0]
