7530a24f69214df90efabeeb03c10e45c952b635
KacperGrabiec <grabiec.kacper01@gmail.com>
2021-10-17 16:18:33 +0200

    Add random data to solve

--- py1.py
+++ py1.py
@@ -1,3 +1,8 @@
+import glob
+import os
+import random
+
+
 def load_data(filename: str):
     file = open(filename, 'r')
     data = []
@@ -8,6 +13,12 @@ def load_data(filename: str):
     return data
 
 
+def print_data(filename: str, data: str):
+    file = open(filename, 'w')
+    file.write(data)
+    file.close()
+
+
 def calc_sqr_vec_len(vector):
     return pow(vector[0], 2) + pow(vector[1], 2)
 
@@ -16,18 +27,10 @@ def sum_vectors(v1, v2):
     return [v1[0] + v2[0], v1[1] + v2[1]]
 
 
-data = load_data('in1.txt')
-n = data[0][0]
-moves = data[1:]
-max_distance = -1
-results = []
-indexes = []
-
-
 def solve():
     for i in range(0, n):
         skip = False
-        start = [0, 0]
+        vector = [0, 0]
         global max_distance
         for j in range(0, len(indexes)):
             if i == indexes[j]:
@@ -38,8 +41,8 @@ def solve():
         else:
             indexes.append(i)
             for j in range(0, len(indexes)):
-                start = sum_vectors(start, moves[indexes[j]])
-            results.append(start)
+                vector = sum_vectors(vector, moves[indexes[j]])
+            results.append(vector)
             distance = calc_sqr_vec_len(results[-1])
             if distance > max_distance:
                 max_distance = distance
@@ -49,5 +52,33 @@ def solve():
             indexes.pop()
 
 
+data = load_data('./input_data/in2.txt')
+n = data[0][0]
+moves = data[1:]
+max_distance = -1
+results = []
+indexes = []
 solve()
 print(max_distance)
+
+
+'''index = 1
+os.chdir('./input_data')
+for file in glob.glob('*.txt'):
+    data = load_data(file)
+    n = data[0][0]
+    moves = data[1:]
+    max_distance = -1
+    results = []
+    indexes = []
+    solve()
+    print(max_distance)
+    #print_data('./output_data/out' + str(index) + '.txt', str(max_distance))
+    #index = index + 1'''
+
+###FILE GENERATOR###
+'''file = open('./input_data/in2.txt', 'w')
+file.write('10\n')
+for i in range(0, 10):
+    file.write(str(random.randint(-10000, 10000)) + ' ' + str(random.randint(-10000, 10000)) + '\n')
+file.close()'''
