6642ad32bb9f4ef4313c955e6bea0c4db942aaaf
unknown <grabiec.kacper01@gmail.com>
2021-10-21 18:46:10 +0200

    Add css to the Raport

--- py2.py
+++ py2.py
@@ -11,19 +11,39 @@ def load_data(directory: str):
     return data
 
 
+def generate_template():
+    template = ''
+    for i in range(0, len(input_files)):
+        template += '''
+        <div class="line">
+            <div class="data">
+        '''
+        for j in input_files[i]:
+            template += f'''
+                <div class="input">{j}</div>
+            '''
+        template += f'''
+            </div>
+            <div class="data">{output_files[i][0]}</div>
+        </div>'''
+    return template
+
+
 def print_data():
     file = open('raport.html', 'w')
     file.write(f'''
     <!DOCTYPE HTML>
     <html lang="pl-PL">
         <head>
-            <meta charset="utf-8"/>
+            <meta charset="utf-8">
             <title>Raport</title>
             <meta name="viewport" content="width=device-width, initial-scale=1">
+
+            <link rel="stylesheet" href="style.css">
         </head>
-        
+
         <body>
-            {}
+            {generate_template()}
         </body>
     </html>
     ''')
